264aa56d957c4a00409621f1b3e7951262c544cc
SEBAMP <seba.mahuzier@gmail.com>
2018-04-11 18:10:48 -0300

    POO
    This reverts commit d75ac5874fcb68e002766bea31bc4e0c1f1f7688.

---
+++ Homework/src/Buzz.java
@@ -0,0 +1,29 @@
+public class Buzz {
+	
+public static void baffle(String blimp) {
+System.out.println(blimp);//4�
+zippo("ping", -5);//5�
+}
+
+public static void zippo(String quince, int flag) {
+if (flag < 0) {
+System.out.println(quince + " zoop");//6�
+} else {
+System.out.println("ik");//2�
+baffle(quince);//3�
+System.out.println("boo-wa-ha-ha");//7�
+}}
+
+
+public static void main(String[] args) {
+zippo("rattle", 13); // 1�
+}
+
+}
+
+//3.-El valor de blimp cuando baffle es invocado es "rattle" 
+//4.- El output es:
+//   ik
+//   rattle
+//   ping zoop
+//   boo-wa-ha-ha
